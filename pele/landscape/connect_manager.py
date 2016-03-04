3226af8e7c1f810337a4d353853de3fc72905d2b
Samuel Niblett <sn402@dexter.cm.cluster>
2016-03-04 10:02:36 +0000

    Added two new connection strategies: one to submit connection jobs which attempt to connect a set of minima specified in a file, and one to attempt to connect all pairs of minima exactly once.

--- pele/landscape/connect_manager.py
+++ pele/landscape/connect_manager.py
@@ -96,25 +96,33 @@ class ConnectManagerCentre(BaseConnectManager):
 
     def _build_list(self):
         if self.verbosity > 0:
-            print "populating list of minima not connected to the global minimum"
+            print "populating list of minima not connected to the central minimum"
         self.minpairs = deque()
 
         central = None
+#        print "self.user_data:", self.user_data
         for min in self.database.minima():
+            print "checking min ", min.id()
             if(min.user_data==self.user_data):
+                print "found central"
                 central = min
                 break
         if central is None:
-            print "Error: no central minimum supplied"
-            return
+            print "Not found central in database, using the first one to be found"
+            central = self.database.getMinimum(1) # If no central minimum is labelled, assume the first one
+        print "Central minimum is ", central.id()
+#            print "Error: no central minimum supplied"
+#            return
 
         graph = TSGraph(self.database)
 
-        for m in self.database.minima()[1:]:
+        for m in self.database.minima():
+            if m == central:
+                print "Skipping central minimum"
+                continue
             if not graph.areConnected(central, m):
                 if self.is_good_pair(central, m):
                     self.minpairs.append((central, m))                    
-
 
 class ConnectManagerUntrap(BaseConnectManager):
     """class to select double ended connect jobs using the untrap strategy
@@ -175,11 +183,13 @@ class ConnectManagerUntrap(BaseConnectManager):
         self.minpairs = deque()
 
         graph = TSGraph(self.database).graph
-        cclist = list(nx.connected_components(graph))
+#        cclist = list(nx.connected_components(graph))
+        cclist = sorted(nx.connected_components(graph), key = len, reverse=True)
 
         # get the largest cluster
         group1 = cclist[0]
         min1 = sorted(group1, key=lambda m: m.energy)[0]
+
         if not min1 == self.database.minima()[0]:
             # make sure that the global minimum is in group1
             print "warning, the global minimum is not the in the largest cluster."
@@ -236,8 +246,8 @@ class ConnectManagerCombine(BaseConnectManager):
         self.minpairs = deque()
 
         graph = TSGraph(self.database).graph
-        cclist = list(nx.connected_components(graph))
-
+#        cclist = list(nx.connected_components(graph))
+        cclist = sorted(nx.connected_components(graph), key = len, reverse=True)
         # remove clusters with fewer than clust_min
         cclist = [cc for cc in cclist if len(cc) >= self.clust_min]
 
@@ -278,6 +288,138 @@ class ConnectManagerCombine(BaseConnectManager):
 
         return self.minpairs
 
+class ConnectManagerSubset(BaseConnectManager):
+    """a class to submit connection jobs until a specified subset of minima are mutually connected"""
+    def __init__(self, database, minlist, mindist=None, list_len=20, verbosity=1):
+#        print "Setting up ConnectManagerSubset"
+        self.database = database
+        self.list_len = list_len
+#        print "setting up self.subset"
+#        print "minimum list"
+#        print minlist
+        self.subset = [database.getMinimum(mid) for mid in minlist]
+#        print "self.subset complete"
+
+        self.mindist = mindist
+        self.verbosity = verbosity
+
+        self.minpairs = deque()          
+
+#        print "ConnectManagerSubset set up correctly"
+
+    def _build_list(self):
+        """make a list of minima pairs to try to connect"""
+
+        print "analyzing the database to find minima to connect"
+        self.minpairs = deque()
+
+#        print "building graph"
+        graph = TSGraph(self.database).graph
+#        print "graph built successfully"
+
+        # Experimental method: rather than simply picking the lowest-energy minimum in each
+        # cluster to use in the connection attempt, we choose the closest pair of minima between the two clusters.
+        if self.mindist is not None:
+            from pele.landscape._distance_graph import _DistanceGraph
+            d_graph = _DistanceGraph(self.database,graph,self.mindist,self.verbosity)
+
+        cclist = sorted(nx.connected_components(graph), key = len, reverse=True)
+        print "There are ", len(cclist), "clusters in total"
+        # Remove any clusters which do not contain a minimum from the original subset
+        cclist = [cc for cc in cclist if any([min in self.subset for min in cc])]
+
+        if len(cclist) == 1:
+            print "all subset minima are connected"
+            return self.minpairs
+        print "There are ", len(cclist), "clusters which contain subset minima in the database"
+
+        # get the group that all other groups will be connected to
+        group1 = cclist[0]
+        min1 = sorted(group1, key=lambda m: m.energy)[0]
+        if True:
+            # make sure that the global minimum is in group1
+            global_min = self.database.minima()[0]
+            if global_min not in group1:
+                print "warning, the global minimum is not the in the largest cluster.  Will try to connect them"
+                self.minpairs.append((min1, global_min))
+
+        # remove group1 from cclist
+        cclist.remove(group1)
+
+        # get a minimum from each of the other groups
+        for group2 in cclist:
+
+            if len(self.minpairs) > self.list_len:
+                break
+
+            if self.verbosity>0:
+                print "adding groups of size", len(group1), "and", len(group2), "to the connect list"
+
+            # sort the groups by energy
+            group2.sort(key=lambda m: m.energy)
+
+            # select the lowest energy minima in the groups
+            # (this can probably be done in a more intelligent way)
+            min2 = group2[0]
+
+            if self.mindist is None:
+
+                if self.is_good_pair(min1, min2):
+                    self.minpairs.append((min1, min2))
+#                print "connecting minima", min1.id(), min2.id()
+
+            else:
+
+                for min in group1:
+                    d_graph.addMinimum(min)
+                for min in group2:
+                    d_graph.addMinimum(min)
+                path, weights = d_graph.shortestPath(min1,min2)
+                
+                count = 0
+                for weight in weights:
+                    count+=1
+                    if weight>1e-6 and weight<1e10:
+                        self.minpairs.append((path[count-1],path[count]))               
+
+        return self.minpairs
+
+class ConnectManagerExhaustive(BaseConnectManager):
+    """Attempt to connect each pair of minima from the initial database exactly once, and no others."""
+
+    def __init__(self, database, verbosity=1):
+        self.database = database
+        self.verbosity = verbosity
+
+        self.N = self.database.number_of_minima()
+        self.minpairs = deque()
+
+        self.used = False
+
+    def _build_list(self):
+
+       self.minpairs = deque()
+
+       if self.used:
+           print "Warning: already attempted all the pairs"
+           return self.minpairs
+
+       if self.verbosity > 0:
+           print "populating list of minimum pairs for the exhaustive connect strategy"
+
+       for m in xrange(self.N):
+           minA = self.database.minima()[m]
+           print "adding pairs involving minimum ", minA.id()
+           count=0
+           for n in xrange(m+1, self.N):
+               minB = self.database.minima()[n]
+               if self.is_good_pair(minA, minB):
+                   self.minpairs.append((minA, minB))
+                   count+=1
+           print "added ", count, "pairs"
+
+       self.used=True
+       return self.minpairs
 
 class ConnectManagerRandom(BaseConnectManager):
     """manager to return random minima to connect"""
@@ -299,6 +441,8 @@ class ConnectManagerRandom(BaseConnectManager):
 
             if min1 == min2: continue
             if self.is_good_pair(min1, min2):
+#                print "coords of min1:"
+#                print min1.coords
                 return min1, min2
 
             #        print "worker requested new job, sending minima", min1.id(), min2.id()
@@ -336,7 +480,7 @@ class ConnectManager(object):
         """raised when the connect manager can't find any more pairs to connect"""
 
     def __init__(self, database, strategy="random", list_len=20, clust_min=4, Emax=None,
-                 untrap_nlevels=20, verbosity=1):
+                 untrap_nlevels=20, verbosity=1, minlist=[], mindist=None):
         self.database = database
         self.default_strategy = strategy
         self.verbosity = verbosity
@@ -345,9 +489,11 @@ class ConnectManager(object):
         self.manager_combine = ConnectManagerCombine(self.database, list_len=list_len, clust_min=4)
         self.manager_untrap = ConnectManagerUntrap(database, list_len=list_len, nlevels=untrap_nlevels)
         self.manager_gmin = ConnectManagerGMin(database, list_len=list_len, verbosity=self.verbosity)
-        self.manager_centre = ConnectManagerCentre(database, list_len=list_len, verbosity=self.verbosity)        
+        self.manager_centre = ConnectManagerCentre(database, list_len=list_len, verbosity=self.verbosity)
+        self.manager_subset = ConnectManagerSubset(database, minlist, mindist=mindist, list_len=list_len)
+        self.manager_exhaustive = ConnectManagerExhaustive(self.database)
 
-        self.possible_strategies = ["random", "combine", "untrap", "gmin", "centre"]
+        self.possible_strategies = ["random", "combine", "untrap", "gmin", "centre", "subset", "exhaustive"]
         self.backup_strategy = "random"
         self._check_strategy(self.backup_strategy)
         self._check_strategy(self.default_strategy)
@@ -359,7 +505,9 @@ class ConnectManager(object):
         self.manager_untrap.set_good_pair_test(self.untried)
         self.manager_gmin.set_good_pair_test(self.untried)
         self.manager_centre.set_good_pair_test(self.untried)
-        
+        self.manager_subset.set_good_pair_test(lambda x,y: True)  # sn402: Need to change this?
+        self.manager_exhaustive.set_good_pair_test(self.untried)
+       
     def _already_tried(self, min1, min2):
 
         if (min1, min2) in self.attempted_list:
@@ -428,6 +576,25 @@ class ConnectManager(object):
             else:
                 if self.verbosity > 0:
                     print "sending a connect job to connect all minima with the central minimum", min1.id(), min2.id()
+
+        if strategy == "subset":
+            min1, min2 = self.manager_subset.get_connect_job()
+            if min1 is None or min2 is None:
+                raise self.NoMoreConnectionsError(
+                    "couldn't find any unconnected pairs from the target subset. Stopping now")
+            else:
+                if self.verbosity > 0:
+                    print "sending a connect job to connect a pair of clusters containing minima from the original subset", min1.id(), min2.id()
+
+        if strategy == "exhaustive":
+            print "Calling exhaustive connect job"
+            min1, min2 = self.manager_exhaustive.get_connect_job()
+            if min1 is None or min2 is None:
+                raise self.NoMoreConnectionsError(
+                    "Completed all pairs from the original database. Stopping now")
+            else:
+                if self.verbosity > 0:
+                    print "sending a connect job to connect all minima from the initial database", min1.id(), min2.id()
 
         if strategy == "random":
             min1, min2 = self.manager_random.get_connect_job()
